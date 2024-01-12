33bf7c51688e4df01500651869620dc12af9a2c6
Lor3nzoRossi <lorenzoistruzione@gmail.com>
2024-01-12 15:00:17 +0100

    2 classi

---
+++ src/grandprix/gruppo4/Auto.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package grandprix.gruppo4;
+
+/**
+ *
+ * @author Studente
+ */
+public class Auto {
+    public int cilindrata;
+    public int potenza;
+    public String marca;
+
+    public Auto(int cilindrata, int potenza, String marca) {
+        this.cilindrata = cilindrata;
+        this.potenza = potenza;
+        this.marca = marca;
+    }
+}
